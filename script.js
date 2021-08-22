3d1255eb3e18477bd9205c7c289706d91e2d917c
MarikaForss <88820019+MarikaForss@users.noreply.github.com>
2021-08-22 20:05:12 +0000

    20210822

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-function laskutehtava(ekaArvo,tokaArvo){
-  return ekaArvo+tokaArvo;
-}
-function pvmfin(vuosi,kk,pv){
-  if(kk==03 || kk==04 || kk==05){
-    return "kevät: " +pv+"."+kk+"."+vuosi;
-}
-else if(kk==06 || kk==07 || k==08){
-  return "kesä: "+pv+"."+kk+"."+vuosi;
-}
-else if(kk==09 || kk==10 || k==11){
-  return "syksy: "+pv+"."+kk+"."+vuosi;
-}
-else if(kk==12 || kk==01 || k==02){
-  return "talvi: "+pv+"."+kk+"."+vuosi;
-}
-
 console.log("Täällä ollaan!");
+
 
 let ika=41;//numerot kirjoitetaan ilman lainausmerkkejä
 ika=ika+1;//integer (int) 
@@ -48,32 +32,13 @@ else{
   console.error("kasva vielä vähän...");
 }
 
-console.log(laskutehtava (123,123));
-
 let pvm= "2021/01/18";
-
-for(let i=1; i<=15; i++){
-  console.log(i+" "+pvmfin(pv.split("/")[0],pv.split("/")[1],pvm.split("/"),pvm.split("/")));
-}
 
 //console.log(document.querySelector('#kenttä').type);
 //console.log(document.querySelector('lisaanappi').value);
 
-document.querySelector('lisaanappi').addEventListener('click', e=>{ e.preventDefault();
-console.log(document.querySelector('#kentta').value);
-
-document.querySelector('.ilmoitus').innerHTML="kentän sisältöon:<b>"+document.querySelector ('#kentta').value+"<b>";
-{
-document.querySelector('merkinta').addEventListener('click', e=>{e.preventDefault();
+document.querySelector('projektinappula').innerHTML=  addEventListener('click', e =>{alert("Täytä kaikki kentät"); 
 });
-
-/*monen
-rivin
-kommentti
-*/
-
-
-
 
 
 
