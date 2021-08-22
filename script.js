323792747531a63594e4f91852748bc918915769
MarikaForss <88820019+MarikaForss@users.noreply.github.com>
2021-08-22 10:42:38 +0000

    20210822

--- script.js
+++ script.js
@@ -1,79 +1,81 @@
 function laskutehtava(ekaArvo,tokaArvo){
-    return ekaArvo +tokaArvo;
+  return ekaArvo+tokaArvo;
 }
-function pvmFin(vuosi,kk,pv){
+function pvmfin(vuosi,kk,pv){
   if(kk==03 || kk==04 || kk==05){
-  return "kevät: "+ +pv+"."+kk+"."+vuosi;   
+    return "kevät: " +pv+"."+kk+"."+vuosi;
 }
-  else if(kk==06 || kk==07 || kk==08){
-  return "kesä: " + +pv+"."+kk+"."+vuosi;
+else if(kk==06 || kk==07 || k==08){
+  return "kesä: "+pv+"."+kk+"."+vuosi;
 }
-  else if (kk==09 || kk==10 || kk==11){
-  return "syksy:"+ +pv+"."+kk+"."+vuosi;
+else if(kk==09 || kk==10 || k==11){
+  return "syksy: "+pv+"."+kk+"."+vuosi;
 }
-  else{
-  return "talvi:"+ +pv+"."+kk+"."+vuosi; ;
-}
-  return "Päivämäärä oikeassa järjestyksessä on: "+pv+"."+kk+"."+vuosi;   
+else if(kk==12 || kk==01 || k==02){
+  return "talvi: "+pv+"."+kk+"."+vuosi;
 }
 
 console.log("Täällä ollaan!");
 
-let ika = 41; //numerot kirjoitetaan ilman lainauismerkkejä
-ika = ika +0; //integer (int)
+let ika=41;//numerot kirjoitetaan ilman lainausmerkkejä
+ika=ika+1;//integer (int) 
 
-let ikakirjaimina = 41; //string (str)
-ikakirjaimina = ikakirjaimina + 0; 
+let ikakirjaimina="41";//string (str)
+ikakirjaimina=ikakirjaimina +1;
+  console.log(ikakirjaimina);
 
-console.log(ikakirjaimina);
+console.log("Sinun ikäsi on +ikä");
 
-console.log("sinun ikäsi on: " +ika);
+let juuvaiei=true; //boolen
+  console.log(juuvaiei);
 
-let juuvaiei = true; //boolean
+let nimi="Marika Forss"
+  console.log("Kirjoita isolla:"+nimi.toUpperCase());
+  console.log("Splittausharjoitus:"+nimi.split("")[0]);
 
-console.log(juuvaiei);
-
-let nimi = "Marika Forss";
-
-console.log("Kirjoita isolla: " + nimi.toUpperCase());
-
-console.log("Splittausharjoitus: " + nimi.split("")[0]);
-
-//While ja for loop:
+  //while ja for loop:
 
 for(let i=0; i<=10; i++){
-  console.log("Loopin iteraatioluku on: " + i);
+  console.log("Loopin interaatioluku on:"+i);
 }
-
-if(ika > 18 || nimi == "Marika Forss"){
- console.log("Onnea, olet täysi-ikäinen ja/ tai olet Marika!");
+if(ika>18 || nimi== "Marika Forss"){
+  console.log("Onnea olet täysi-ikäinen ja/tai olet Marika!");
 }
-else if (ika === 18 && nimi == "Keijo"){
-  console.log("Onnea Keijo, olet juri ja juuri täysi-ikäinen!");
+else if(ika=== 18 && nimi== "Keijo"){
+  console.log("Onnea Keijo, olet juuri ja juuri täysi ikäinen!");
 }
 else{
   console.error("kasva vielä vähän...");
 }
 
-console.log(laskutehtava(123,123));
+console.log(laskutehtava (123,123));
 
-let pvm= "2021/09/18";
+let pvm= "2021/01/18";
 
-for(let i=0; i<15; i++){
-console.log(i+" "+pvmFin(pvm.split("/")[0],pvm.split("/")[1],pvm.split("/")[2]));
+for(let i=1; i<=15; i++){
+  console.log(i+" "+pvmfin(pv.split("/")[0],pv.split("/")[1],pvm.split("/"),pvm.split("/")));
 }
 
+//console.log(document.querySelector('#kenttä').type);
+//console.log(document.querySelector('lisaanappi').value);
 
+document.querySelector('lisaanappi').addEventListener('click', e=>{ e.preventDefault();
+console.log(document.querySelector('#kentta').value);
 
+document.querySelector('.ilmoitus').innerHTML="kentän sisältöon:<b>"+document.querySelector ('#kentta').value+"<b>";
+{
+document.querySelector('merkinta').addEventListener('click', e=>{e.preventDefault();
+});
 
-
- 
-
-
-
-
-/*
-Monen
+/*monen
 rivin
 kommentti
 */
+
+
+
+
+
+
+
+
