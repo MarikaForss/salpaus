5e7611692d638952fbb4c873b34ce100c58c3b10
MarikaForss <88820019+MarikaForss@users.noreply.github.com>
2021-08-23 09:30:12 +0000

    20210823

--- script.js
+++ script.js
@@ -48,11 +48,22 @@ setTimeout(()=> document.querySelector('.ilmoitus').innerHTML='',3000);
 document.querySelector('.merkinta').addEventListener('click', e =>{
   e.preventDefault();
   console.log("nappi toimii");
-
+});
 
 if(document.querySelector('#teht_a').checked){document.querySelector('.boksit').children[0].innerHTML = '<li class="sisalto"><input type="checkbox" id="teht_a" checked> Tehtävä A on tehty!</li>';
+
 }
-else{document.querySelector}
+else{
+  document.querySelector('.boksit').children[0].innerHTML = '<li class= "sisalto"><input type="checkbox" id=teht_a"> Tehtävä A</li>';
+
+}
+
+if(document.querySelector('#teht_b').checked){document.querySelector('.boksit').children[1].innerHTML = '<li class="sisalto"><input type="checkbox" id="teht_b" checked> Tehtävä B on tehty!</li>';
+}
+
+else{
+ document.querySelector('.boksit').children[1].innerHTML = '<li class= "sisalto"><input type="checkbox" id=teht_b"> Tehtävä B</li>';
+}
 
 
 
