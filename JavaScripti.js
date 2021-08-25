17761de7ed50147dfacb13dd57748c1d4fd8910a
MarikaForss <88820019+MarikaForss@users.noreply.github.com>
2021-08-25 10:34:01 +0000

    20212508

--- JavaScripti.js
+++ JavaScripti.js
@@ -4,9 +4,20 @@ document.querySelector('#projektinappula').addEventListener ('click', e =>{
 e.preventDefault();
 console.log(document.querySelector('#lisaamerkinta').value);
 
-document.querySelector('.ilmoitus').innerHTML ="Täytä kaikki kentät:"+ document.querySelector('#lisaamerkinta').value+ "</b>";
+document.querySelector('.ilmoitus').innerHTML ="Täytä kaikki kentät"
 
 setTimeout(()=> document.querySelector('.ilmoitus').innerHTML='',3000);
 
-})
+});
 
+document.querySelector('#nappulanappula').addEventListener('click', e =>{
+ e.preventDefault();
+
+
+if(document.querySelector('#html').checked){document.querySelector('.projektivalitsin').children[0].innerHTML = '<li class=".projektinsisalto"><input type="checkbox" id="html"checked> HTML SUORITETTU!</li>';
+}
+else{
+document.querySelector('.projektivalitsin').children[0].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id=html">HTML</li>';
+}
+
+});
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # salpaus
+Perjantaina 20.8 sain harjoitukset tehtyä ja kaikki toimi ohjelmointi.html sivulla, kotona harjoitellessa script.js sivu tyhjeni kokonaan, en tiedä mitä tapahtui! 
+
+Maanantaina 23.8 ohjelmointi.html taas toimii!
