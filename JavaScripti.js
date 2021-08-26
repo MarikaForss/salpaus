771024803ef43ae1a2ba80138b4d7ef9c690d1e6
MarikaForss <88820019+MarikaForss@users.noreply.github.com>
2021-08-26 11:20:37 +0000

    20210826

--- JavaScripti.js
+++ JavaScripti.js
@@ -2,37 +2,45 @@
 
 document.querySelector('#projektinappula').addEventListener ('click', e =>{
 e.preventDefault();
-console.log(document.querySelector('#lisaamerkinta').value);
 
 document.querySelector('.ilmoitus').innerHTML ="Täytä kaikki kentät"
 
 setTimeout(()=> document.querySelector('.ilmoitus').innerHTML='',3000);
-
 });
+
 document.querySelector('#nappulanappula').addEventListener('click', e =>{
  e.preventDefault();
 
-if(document.querySelector('#html').checked){document.querySelector('.projektivalitsin').children[0].innerHTML = '<li class=".projektinsisalto"><input type="checkbox" id="html"checked> HTML on suoritettu!</li>';
+if(document.querySelector('#html').checked){document.querySelector('.projektivalitsin').children[0].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id="html"checked> HTML on suoritettu!</li>';
 }
 else{
-document.querySelector('.projektivalitsin').children[0].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id=html"> HTML</li>';
+document.querySelector('.projektivalitsin').children[0].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id="html"> HTML</li>';
 }
-if(document.querySelector('#css').checked){document.querySelector('.projektivalitsin').children[1].innerHTML = '<li class=".projektinsisalto"><input type="checkbox" id="css"checked> CSS on suoritettu!</li>';
-}
-else{
- document.querySelector('.projektivalitsin').children[1].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id=css"> CSS</li>';
-}
-if(document.querySelector('#javascript').checked){document.querySelector('.projektivalitsin').children[2].innerHTML = '<li class=".projektinsisalto"><input type="checkbox" id="javascript"checked> JavaScript on suoritettu!</li>';
+if(document.querySelector('#css').checked){document.querySelector('.projektivalitsin').children[1].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id="css"checked> CSS on suoritettu!</li>';
 }
 else{
- document.querySelector('.projektivalitsin').children[2].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id=javascript">JavaScript</li>';
+document.querySelector('.projektivalitsin').children[1].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id="css"> CSS</li>';
 }
-if(document.querySelector('#cicd').checked){document.querySelector('.projektivalitsin').children[3].innerHTML = '<li class=".projektinsisalto"><input type="checkbox" id="cicd"checked> CI/CD on suoritettu!</li>';
+if(document.querySelector('#javascript').checked){document.querySelector('.projektivalitsin').children[2].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id="javascript"checked> JavaScript on suoritettu!</li>';
 }
 else{
- document.querySelector('.projektivalitsin').children[3].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id=cicd">CI/CD</li>';
+document.querySelector('.projektivalitsin').children[2].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id="javascript">JavaScript</li>';
+}
+if(document.querySelector('#cicd').checked){document.querySelector('.projektivalitsin').children[3].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id="cicd"checked> CI/CD on suoritettu!</li>';
+}
+else{
+document.querySelector('.projektivalitsin').children[3].innerHTML = '<li class="projektinsisalto"><input type="checkbox" id="cicd">CI/CD</li>';
 }
 });
+
+document.querySelector('#projektinappula').addEventListener ('click', e =>{
+e.preventDefault();
+
+
+if(document.querySelector('.kentta').innerHTML= document.querySelector('.laatikonohje').value
+
+
+
 
 
 
